7bc6626b05dc508d2ea2a742f64fccb7b2f03f9c
Willsmaith Pochette <pwillsmaith@gmail.com>
2023-07-06 09:41:59 -0400

    added character component and route

---
+++ Guided2/react-app/src/component/film.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom'
+const baseUrl = `http://localhost:4000/api`
+
+ function Film(props){
+    const[Film, setFilm] = useState([]);
+
+    async function getFilm(){
+        var film = await fetchFilm()
+        setFilm(film);
+    }
+    async function fetchFilm() {
+        var FilmUrl = `${baseUrl}/films/${props.id}`;
+        return await fetch(FilmUrl)
+      .then(res => res.json())
+  }
+
+
+    useEffect(() => {getFilm()}, []);
+
+        return (
+        <div>
+        <h1 id="movName"></h1>
+        <section id="generalInfo">
+      <p>Released: <span id="released"></span></p>
+      <p>Director: <span id="director"></span></p>
+      <p>Episode: <span id="episode"></span></p>
+        </section>
+        <section id="characters">
+      <h2>Characters</h2>
+      <ul></ul>
+        </section>
+        <section id="planets">
+      <h2>Planets</h2>
+      <ul></ul>
+        </section>
+        </div>
+        )
+}
+
+
+export default Film;
