cbe0998c0b3b90927baf7ce5aafe4926dfaeab82
Willsmaith Pochette <pwillsmaith@gmail.com>
2023-07-12 14:26:50 -0400

    fix git problem

--- Guided2/react-app/src/App.js
+++ Guided2/react-app/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Characters from './component/characters';
 import Character from './component/character';
-<<<<<<< Updated upstream
 import { Route,Routes } from 'react-router-dom';
 import Film from './component/film';
 
@@ -15,10 +14,6 @@ function App() {
           <Route path ="/Characters/:id" element= {<Character/>}/>
           <Route path = "/Film/:id" element = {<Film/>}/>
         </Routes>
-=======
-      <Character id = "1"/>
-
->>>>>>> Stashed changes
     </div>
   );
 }
