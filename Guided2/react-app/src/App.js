da1b2ad6e1bb8ba83f4eb62bdb232d74021a69cc
Willsmaith Pochette <pwillsmaith@gmail.com>
2023-07-12 14:21:51 -0400

    unstash change

--- Guided2/react-app/src/App.js
+++ Guided2/react-app/src/App.js
@@ -3,8 +3,7 @@ import Characters from './component/characters';
 import Character from './component/character';
 <<<<<<< Updated upstream
 import { Route,Routes } from 'react-router-dom';
-=======
->>>>>>> Stashed changes
+import Film from './component/film';
 
 
 function App() {
@@ -14,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path ="/" element= {<Characters/>}/>
           <Route path ="/Characters/:id" element= {<Character/>}/>
+          <Route path = "/Film/:id" element = {<Film/>}/>
         </Routes>
 =======
       <Character id = "1"/>
--- Guided2/react-app/src/component/character.js
+++ Guided2/react-app/src/component/character.js
@@ -1,43 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
-import Films from './films';
 import { useParams } from 'react-router-dom';
 const baseUrl = `http://localhost:4000/api`
 
-/*
-function Homeworld(props){
-    const [Homeworld, setHomeworld] = useState([]);
 
-    async function getHomeworld(){
-        var homeworld = await fetchHomeworld()
-        setHomeworld(homeworld);
-    }
-    async function fetchHomeworld() {
-        var HomeworldUrl = `${baseUrl}/planets/${props.homeworld}`;
-        return await fetch(HomeworldUrl)
-      .then(res => res.json())
-    }
-
-    useEffect(() => {getHomeworld()},[]);
-
-    console.log(Homeworld)
-
-    return (
-        <section id="planets">
-            <h2>Homeworld</h2>
-            <p><span id="homeworld">{Homeworld.name}</span></p>
-        </section>
-    )
-     case 'findCharacterFilms':
-            collection = db.collection("films_characters");
-            const characterFilms = await collection.find({"character_id": Number(parameters.id)}).toArray();
-            callback(characterFilms);
-            break;
-
-}*/
- function Character(props){
+ export default function Character(props){
     const[character, setCharacter] = useState([]);
-    character.HomeworldNname = "";
     const[films, setFilms] = useState({});
     const{id} = useParams();
 
@@ -50,7 +18,7 @@ function Homeworld(props){
     async function getCharacter(){
         var character = await fetchCharacter()
         var Homeworld = await fetchHomeworld(character)
-        character.HomeworldName = Homeworld.name;
+        character.Homeworld = Homeworld;
         setCharacter(character);
     }
     async function fetchCharacter() {
@@ -74,7 +42,7 @@ function Homeworld(props){
             </section>
             <section id="planets">
             <h2>Homeworld</h2>
-            <p><span id="homeworld">{character.HomeworldName}</span></p>
+            <Link to ={`/Characters/${character.homeworld}`} ><p><span id="homeworld">{character.Homeworld?.name}</span></p></Link>
             </section>
             <section id="films">
             <h2>Films appeared in</h2>
@@ -87,4 +55,29 @@ function Homeworld(props){
 }
 
 
-export default Character;
+function Films(props){
+    const[films, setFilms] = useState([]);
+    async function getFilms(){
+        var films = await fetchFilms()
+        setFilms(films);
+    }
+
+    async function fetchFilms() {
+        var filmsUrl = `${baseUrl}/characters/${props.id}/films`;
+        return await fetch(filmsUrl)
+      .then(res => res.json())
+  }
+    useEffect(() => {getFilms()}, []);
+    
+    console.log(films)
+    const FilmsHtml = films.map((film) =>
+        <div><Link to = {`/Film/${film.id}`}>{film.title}</Link></div>);
+
+        return (
+        <div>
+            {
+            FilmsHtml
+            }
+        </div>
+        )
+}
